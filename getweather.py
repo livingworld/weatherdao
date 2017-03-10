1f76eb5c58987f28df65b23bc692eb5e64a697cb
livingworld <455412741@qq.com>
2017-03-10 20:05:19 +0800

    revise

--- getweather.py
+++ getweather.py
@@ -20,7 +20,8 @@ def dumpTianqi(result):
             #weatherstring=weatherstring+item1[0]+item1[1]+'  '#通过这一步可以实现
     #weatherstring1=weatherstring+'\n'#查询记录换行，方便历史查询
     history_weather_list.append(weatherstring)#每次查询的记录保存在历史天气查询list当中
-    return weatherstring#返回天气查询的记录
+    #return weatherstring#返回天气查询的记录
+    print(weatherstring)
 
 '''result = requests.get(url, params, timeout)，发送get请求'''
 def fetchWeather(location):
@@ -34,9 +35,10 @@ def fetchWeather(location):
         'location': location,
         'language': LANGUAGE,
         'unit': UNIT
-    },verify=False)
+    })
     result = result.text
     result = json.loads(result)
+    print(result)
     return result
 
 def history_weather():
@@ -53,3 +55,11 @@ def help_weather():
 
 def quit_weather():
     return quit()
+
+if __name__ == "__main__":
+    location = input("请输入：")
+    result = fetchWeather(location)
+    if location == "help":
+        history_weather()
+    else:
+        print(result)
